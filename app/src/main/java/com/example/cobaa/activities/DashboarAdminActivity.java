1354d4fcfe7809afc667af0b4deb13dbe592b9cd
fend252 <efendie292@gmail.com>
2021-08-13 23:35:06 +0700

    add login admin

---
+++ app/src/main/java/com/example/cobaa/activities/DashboarAdminActivity.java
@@ -0,0 +1,14 @@
+package com.example.cobaa.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboarAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboar_admin);
+    }
+}
