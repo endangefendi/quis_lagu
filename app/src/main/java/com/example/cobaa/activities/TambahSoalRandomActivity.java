cd70503ba995b5947bd904e56105e7eb26c7ac0e
fend252 <efendie292@gmail.com>
2021-08-15 13:21:43 +0700

    create soal random save db

--- app/src/main/java/com/example/cobaa/activities/TambahSoalRandomActivity.java
+++ app/src/main/java/com/example/cobaa/activities/TambahSoalRandomActivity.java
@@ -2,6 +2,7 @@ package com.example.cobaa.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -10,27 +11,45 @@ import android.os.Bundle;
 import android.os.FileUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cobaa.R;
+import com.example.cobaa.models.BannerModel;
+import com.example.cobaa.models.SoalModel;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
 import java.net.URISyntaxException;
 
 public class TambahSoalRandomActivity extends AppCompatActivity {
 
     TextView name_file;
     TextView ket;
+
+    String path = null;
+
+    private final FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+    private final StorageReference storageReference = firebaseStorage.getReference();
+    private ProgressDialog progressDialog;
+
+    EditText txt_soal, txt_jawaban1, txt_jawaban2, txt_jawaban3, txt_jawaban4, txt_jawaban_benar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_soal_random);
         name_file = findViewById(R.id.name_file);
-        if (name_file.getText().toString().trim().equalsIgnoreCase("")){
+        if (name_file.getText().toString().trim().equalsIgnoreCase("")) {
             name_file.setVisibility(View.GONE);
-        }else {
+        } else {
             name_file.setVisibility(View.VISIBLE);
         }
         LinearLayout klik = findViewById(R.id.ln_add);
@@ -40,7 +59,21 @@ public class TambahSoalRandomActivity extends AppCompatActivity {
 
         ket = findViewById(R.id.ket);
 
+        txt_soal = findViewById(R.id.txt_soal);
+        txt_jawaban1 = findViewById(R.id.txt_jawaban1);
+        txt_jawaban2 = findViewById(R.id.txt_jawaban2);
+        txt_jawaban3 = findViewById(R.id.txt_jawaban3);
+        txt_jawaban4 = findViewById(R.id.txt_jawaban4);
+        txt_jawaban_benar = findViewById(R.id.txt_jawaban_benar);
 
+        Button btn_save = findViewById(R.id.btn_add);
+        btn_save.setOnClickListener(v -> validasi());
+
+        Button btn_clear = findViewById(R.id.btn_clear);
+        btn_clear.setOnClickListener(v -> clear());
+
+        Button btn_cancel = findViewById(R.id.btn_cancel);
+        btn_cancel.setOnClickListener(v -> batal());
     }
 
     private void showFileChooser() {
@@ -68,21 +101,20 @@ public class TambahSoalRandomActivity extends AppCompatActivity {
                     Uri uri = data.getData();
                     Log.e("", "File Uri: " + uri.toString());
                     // Get the path
-                    String path = null;
                     try {
                         path = getPath(this, uri);
                         Log.e("onActivityResult", "File Path: " + path);
-                        String filename=path.substring(path.lastIndexOf("/")+1);
-                        String extension = path.substring(path.lastIndexOf(".")+1);
-                        if (!extension.equalsIgnoreCase("mp3")){
+                        String filename = path.substring(path.lastIndexOf("/") + 1);
+                        String extension = path.substring(path.lastIndexOf(".") + 1);
+                        if (!extension.equalsIgnoreCase("mp3")) {
                             ket.setText("File tidak didukung");
                             ket.setTextColor(getResources().getColor(R.color.colorPrimary));
-                            name_file.setText("FIle Name: "+filename);
+                            name_file.setText("FIle Name: " + filename);
                             name_file.setVisibility(View.VISIBLE);
-                        }else {
+                        } else {
                             ket.setText("Input audio");
                             ket.setTextColor(getResources().getColor(R.color.abu));
-                            name_file.setText("FIle Name: "+filename);
+                            name_file.setText("FIle Name: " + filename);
                             name_file.setVisibility(View.VISIBLE);
                         }
 
@@ -100,7 +132,7 @@ public class TambahSoalRandomActivity extends AppCompatActivity {
 
     public static String getPath(Context context, Uri uri) throws URISyntaxException {
         if ("content".equalsIgnoreCase(uri.getScheme())) {
-            String[] projection = { "_data" };
+            String[] projection = {"_data"};
             Cursor cursor = null;
 
             try {
@@ -112,12 +144,110 @@ public class TambahSoalRandomActivity extends AppCompatActivity {
             } catch (Exception e) {
                 // Eat it
             }
-        }
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
             return uri.getPath();
         }
 
         return null;
     }
+
+    private void clear() {
+        txt_soal.setText("");
+        txt_jawaban_benar.setText("");
+        txt_jawaban1.setText("");
+        txt_jawaban2.setText("");
+        txt_jawaban3.setText("");
+        txt_jawaban4.setText("");
+        path = null;
+    }
+
+    private void batal() {
+        txt_soal.setText("");
+        txt_jawaban_benar.setText("");
+        txt_jawaban1.setText("");
+        txt_jawaban2.setText("");
+        txt_jawaban3.setText("");
+        txt_jawaban4.setText("");
+        path = null;
+        finish();
+    }
+
+    private void validasi() {
+        String soal = txt_soal.getText().toString().trim();
+        String jawaban_benar = txt_jawaban_benar.getText().toString().trim();
+        String jawaban1 = txt_jawaban1.getText().toString().trim();
+        String jawaban2 = txt_jawaban2.getText().toString().trim();
+        String jawaban3 = txt_jawaban3.getText().toString().trim();
+        String jawaban4 = txt_jawaban4.getText().toString().trim();
+
+        if(soal.isEmpty() && jawaban_benar.isEmpty() && jawaban1.isEmpty() && jawaban2.isEmpty() && jawaban3.isEmpty()
+                    && jawaban4.isEmpty()){
+            Snackbar.make(txt_soal, "Data belum lengkap", Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(soal.isEmpty()){
+            txt_soal.setError( "Soal tidak boleh kosong");
+            return;
+        }
+
+        if(jawaban_benar.isEmpty()){
+            txt_jawaban_benar.setError( "Kunci Jawaban tidak boleh kosong");
+            return;
+        }
+
+        if(jawaban1.isEmpty()){
+            txt_jawaban1.setError( "Pilihan Jawaban 1 tidak boleh kosong");
+            return;
+        }
+        if(jawaban2.isEmpty()){
+            txt_jawaban2.setError( "Pilihan Jawaban 2 tidak boleh kosong");
+            return;
+        }
+        if(jawaban3.isEmpty()){
+            txt_jawaban3.setError( "Pilihan Jawaban 3 tidak boleh kosong");
+            return;
+        }
+        if(jawaban4.isEmpty()){
+            txt_jawaban4.setError( "Pilihan Jawaban 4 tidak boleh kosong");
+            return;
+        }
+
+        saving(soal, jawaban_benar, jawaban1, jawaban2, jawaban3, jawaban4);
+    }
+
+    private void saving( String soal, String jawaban_benar, String jawaban1, String jawaban2, String jawaban3, String jawaban4) {
+        if (path != null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Saving Data");
+            progressDialog.show();
+            Uri file = Uri.fromFile(new File(path));
+            final StorageReference storageReferences = storageReference.child("lagu/" + file.getLastPathSegment());
+            storageReferences.putFile(file)
+                    .addOnSuccessListener(taskSnapshot -> storageReferences.getDownloadUrl().addOnSuccessListener(uri -> {
+                        progressDialog.dismiss();
+                        String id = FirebaseDatabase.getInstance().getReference("soal").push().getKey();
+                        SoalModel upload = new SoalModel(id, jawaban_benar, "random", String.valueOf(uri),
+                                "", jawaban1, jawaban2, jawaban3, jawaban4, soal);
+                        FirebaseDatabase.getInstance().getReference("soal").push().setValue(upload);
+                        Toast.makeText(TambahSoalRandomActivity.this,
+                                "Saving Data successfully", Toast.LENGTH_SHORT).show();
+                        finish();
+                    })).addOnFailureListener(e -> {
+                progressDialog.dismiss();
+                Toast.makeText(TambahSoalRandomActivity.this, e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }).addOnProgressListener(taskSnapshot -> {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred()) /
+                        taskSnapshot.getTotalByteCount();
+                progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+            });
+        } else {
+            Toast.makeText(this, "Make sure all data is correct",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
 }
--- app/src/main/java/com/example/cobaa/models/SoalModel.java
+++ app/src/main/java/com/example/cobaa/models/SoalModel.java
@@ -1,16 +1,32 @@
 package com.example.cobaa.models;
 
 public class SoalModel {
-    String dari_daerah, jenis_soal, id, jawaban, lagu, pilihan1, pilihan2, pilihan3, pilihan4, soal;
+    String id;
+    String jawaban;
+    String jenis_soal;
+    String lagu;
+    String map;
+    String pilihan1;
+    String pilihan2;
+    String pilihan3;
+    String pilihan4;
+    String soal;
 
-
-    public SoalModel(String dari_daerah, String jenis_soal, String id, String jawaban,
-                     String lagu, String pilihan1, String pilihan2, String pilihan3, String pilihan4, String soal){
-        this.dari_daerah = dari_daerah;
-        this.jenis_soal = jenis_soal;
+    public SoalModel(String id,
+            String jawaban,
+            String jenis_soal,
+            String lagu,
+            String map,
+            String pilihan1,
+            String pilihan2,
+            String pilihan3,
+            String pilihan4,
+            String soal){
         this.id = id;
         this.jawaban = jawaban;
+        this.jenis_soal = jenis_soal;
         this.lagu = lagu;
+        this.map = map;
         this.pilihan1 = pilihan1;
         this.pilihan2 = pilihan2;
         this.pilihan3 = pilihan3;
@@ -20,22 +36,6 @@ public class SoalModel {
 
     public SoalModel(){
 
-    }
-
-    public String getDari_daerah() {
-        return dari_daerah;
-    }
-
-    public void setDari_daerah(String dari_daerah) {
-        this.dari_daerah = dari_daerah;
-    }
-
-    public String getJenis_soal() {
-        return jenis_soal;
-    }
-
-    public void setJenis_soal(String jenis_soal) {
-        this.jenis_soal = jenis_soal;
     }
 
     public String getId() {
@@ -54,12 +54,28 @@ public class SoalModel {
         this.jawaban = jawaban;
     }
 
+    public String getJenis_soal() {
+        return jenis_soal;
+    }
+
+    public void setJenis_soal(String jenis_soal) {
+        this.jenis_soal = jenis_soal;
+    }
+
     public String getLagu() {
         return lagu;
     }
 
     public void setLagu(String lagu) {
         this.lagu = lagu;
+    }
+
+    public String getMap() {
+        return map;
+    }
+
+    public void setMap(String map) {
+        this.map = map;
     }
 
     public String getPilihan1() {
